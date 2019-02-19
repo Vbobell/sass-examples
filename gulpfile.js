598afc6806e1e3be7ab621aaac32414441a14812
Vbobell <vinicius.bobel41@gmail.com>
2019-02-19 09:30:02 -0300

    Changed gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,33 @@ const gulpConfig = JSON.parse(fs.readFileSync("gulp-config.json"));
 
 gulpSass.compiler = require('node-sass');
 
-gulp.task('clean', () => {
-    return gulp.src(gulpConfig.cleanFiles.src, {read: false})
-    .pipe(gulpClean());
+gulp.task('clean',async () => {
+    await new Promise((resolve, reject) => {
+        gulp.src(gulpConfig.cleanFiles.src, {read: false})
+        .pipe(gulpClean());
+
+        return resolve();
+    });
 });
 
-gulp.task('concat', () => {
-    return gulp.src(gulpConfig.concatFiles.src)
-    .pipe(gulpConcat(gulpConfig.concatFiles.file))
-    .pipe(gulp.dest(gulpConfig.concatFiles.dest));
+gulp.task('concat', async () => {
+    await new Promise( async (resolve, reject) => {
+        gulp.src(gulpConfig.concatFiles.src)
+        .pipe(gulpConcat(gulpConfig.concatFiles.file))
+        .pipe(gulp.dest(gulpConfig.concatFiles.dest));
+
+        return resolve();
+    });
 });
 
-gulp.task('sass', () => {
-    return gulp.src(gulpConfig.sassFiles.src)
-    .pipe(gulpSass().on('error', gulpSass.logError))
-    .pipe(gulp.dest(gulpConfig.sassFiles.dest));
+gulp.task('sass', async () => {
+    await new Promise((resolve, reject) => {
+         gulp.src(gulpConfig.sassFiles.src)
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(gulp.dest(gulpConfig.sassFiles.dest));
+
+        return resolve();
+    });
 });
 
 gulp.task('default', gulp.series('clean', 'concat', 'sass'));
