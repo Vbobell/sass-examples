ab973b0c51c3d6ecfaec204c72fc861a81edbc26
Vbobell <vinicius.bobel41@gmail.com>
2019-04-29 09:34:43 -0300

    SASS process modified

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,11 @@ const gulpConfig = JSON.parse(fs.readFileSync("gulp-config.json"));
 
 gulpSass.compiler = require('node-sass');
 
-gulp.task('concat', async () => {
+gulp.task('sass', async () => {
     await new Promise( async (resolve, reject) => {
         gulp.src(gulpConfig.concatFiles.src)
         .pipe(gulpConcat(gulpConfig.concatFiles.file))
-        .pipe(gulp.dest(gulpConfig.concatFiles.dest));
-
-        return resolve();
-    });
-});
-
-gulp.task('sass', async () => {
-    await new Promise((resolve, reject) => {
-         gulp.src(gulpConfig.sassFiles.src)
+        .pipe(gulp.dest(gulpConfig.concatFiles.dest))
         .pipe(gulpSass().on('error', gulpSass.logError))
         .pipe(gulp.dest(gulpConfig.sassFiles.dest));
 
@@ -27,5 +19,5 @@ gulp.task('sass', async () => {
     });
 });
 
-gulp.task('default', gulp.series('concat', 'sass'));
+gulp.task('default', gulp.series('sass'));
 
